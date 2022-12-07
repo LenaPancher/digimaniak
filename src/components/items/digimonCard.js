ab5de6abbe8b6e1b87a7e3a6c928aefaa55b4d00
Robin Littiere <72390360+robLittiere@users.noreply.github.com>
2022-12-07 15:01:26 +0100

    added digimon card component starter

---
+++ src/components/items/digimonCard.js
@@ -0,0 +1,16 @@
+import {View, Text} from 'react-native';
+import React, {useCallback} from 'react';
+
+const digimonCard = () => {
+  const digimonImage = useState('');
+
+  const getDigimonImage = useCallback(() => {});
+
+  return (
+    <View>
+      <Text>digimonCard</Text>
+    </View>
+  );
+};
+
+export default digimonCard;
