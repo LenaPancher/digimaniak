28f76055deb697ce7f1ace9446d2dcafb127dc9f
Robin Littiere <72390360+robLittiere@users.noreply.github.com>
2022-12-07 09:13:05 +0100

    3 screens added

---
+++ screens/SupportScreen.js
@@ -0,0 +1,12 @@
+import {View, Text, SafeAreaView} from 'react-native';
+import React from 'react';
+
+const SupportScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>SupportScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SupportScreen;
